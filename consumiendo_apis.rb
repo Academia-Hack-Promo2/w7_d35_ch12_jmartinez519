b0452a6ad18438bcadddc2f3beb415fa29a6c51a
Jonathan Martinez <jamm@jmartinezm.org>
2015-04-09 15:56:57 -0430

    Encasulado el consumo de cada API de noticias en la clase Feeds

--- consumiendo_apis.rb
+++ consumiendo_apis.rb
@@ -19,42 +19,113 @@
 require 'httparty'
 require 'colorize'
 
+class Feed
+  def initialize
+    @newsarr = []
+  end
 
+  def new site
+    case
+      when site == "reddit"
+        @r = Reddit.new
+        @formato_reddit = @r.format @r.news
+        @newsarr.push(@formato_reddit)
+        puts @newsarr.first
+      when site == "mashable"
+        @m = Mashable.new
+        @formato_mashable = @m.format @m.news
+        @newsarr.push(@formato_mashable)
+        puts @newsarr
+      when site == "digg"
+        @d = Digg.new
+        @formato_digg = @d.format @d.news
+        @newsarr.push(@formato_digg)
+        puts @newsarr
+      else
+      return "Error en el nombre del site"
+    end
+  end
+
+  # def author name
+  #  # arr.map { |a| 2*a }
+  #   @newsarr.map do |new| 
+  #     if new["author"] == name
+  #     end
+  #   end 
+  # end
+
+end
 
 class Reddit
   include HTTParty
   base_uri 'http://www.reddit.com/.json?limit=20'
 
 
-  def news
-    puts "=====================================================================".colorize(:red)
-    puts "\t\t\t\tReddit".colorize(:red)
-    response = self.class.get('')
-    response["data"]["children"].each do |new|
-    puts "#{new["data"]["author"]}".colorize(:blue)  
-    puts "#{new["data"]["title"]} "
-    puts "#{Time.at(new["data"]["created_utc"])}"
-    puts "#{"https://www.reddit.com"+new["data"]["permalink"]}\n\n"
+    def news
+      response = self.class.get('')
+      return response
     end
+
+  def format response
+    noticias = []
+      response["data"]["children"].each do |new|
+        res = {}      
+        res["autor"] = new["data"]["author"]
+        res["title"] = new["data"]["title"]
+        res["date"] = Time.at(new["data"]["created_utc"])
+        res["link"] = new["data"]["url"]
+        res["feed"] = "reddit" 
+        noticias.push(res)
+      end
+    return noticias
   end
 end
 
 class Mashable
   include HTTParty
-  base_uri 'http://www.reddit.com/.json?limit=10'
+  base_uri 'http://mashable.com/stories.json'
 
 
   def news
-    puts "=====================================================================".colorize(:red)
-    puts "\t\t\t\tMashable".colorize(:red)
+    response = self.class.get('')
+    return response
+  end
 
-    response = HTTParty.get("http://mashable.com/stories.json?limit=10")
+  def format response
+    noticias = []
     response["new"].each do |new|
-    puts "#{new["author"]}".colorize(:blue)  
-    puts "#{new["title"]} "
-    puts "#{new["post_date"]}"
-    puts "#{new["link"]}\n\n"
+      res = {}
+      res["author"] = new["author"]  
+      res["title"] = new["title"]
+      res["date"] = new["post_date"]
+      res["link"] = new["link"]
+      res["feed"] = "mashable"
+      noticias.push(res)
     end
+
+# Verificar como haher merge de todas las noticias
+# de Mashable
+
+    # response["hot"].each do |new|
+    #   resn = {}
+    #   resn["author"] = new["author"]  
+    #   resn["title"] = new["title"]
+    #   resn["date"] = new["post_date"]
+    #   resn["link"] = new["link"]
+    #   resn["feed"] = "mashable"
+    #   noticias.push(resn)
+    # end
+
+    # response["rising"].each do |new|
+    #   ress = {}
+    #   ress["author"] = new["author"]  
+    #   ress["title"] = new["title"]
+    #   ress["date"] = new["post_date"]
+    #   ress["link"] = new["link"]
+    #   ress["feed"] = "mashable"
+    #   noticias.push(ress)
+    # end
+    return noticias
   end
 end
 
@@ -64,26 +135,35 @@ class Digg
 
 
   def news
-    puts "=====================================================================".colorize(:red)
-    puts "\t\t\t\tDigg".colorize(:red)
+    response = self.class.get('')
+    return response
+  end
 
-    response = HTTParty.get("https://digg.com/api/news/popular.json")
+  def format response
+    noticias = []
     response["data"]["feed"].each do |new|
-    puts "#{new["content"]["author"]}".colorize(:blue)  
-    puts "#{new["content"]["title_alt"]}"
-    puts "#{Time.at(new["date_published"])}"
-    puts "#{new["content"]["url"]}\n\n"
+      res = {}
+      res["author"] = new["content"]["author"] 
+      res["title"] = new["content"]["title_alt"]
+      res["date"] = Time.at(new["date_published"])
+      res["link"] = new["content"]["url"]
+      res["feed"] = "digg"
+      noticias.push(res)
     end
+    return noticias
   end
 end
 
 def main
-  reddit_feed = Reddit.new
-  reddit_feed.news
-  mashable_feed = Mashable.new
-  mashable_feed.news
-  digg_feed = Digg.new
-  digg_feed.news
+  while true
+    init = Feed.new
+    puts "¿Que sitio de noticias desea ver? Opciones: reddit, mashable, digg.".colorize(:blue)
+    site = gets.chomp.to_s.downcase
+    puts init.new site
+    puts "¿Que author quieres ver?"
+    # author = gets.chomp.to_s.downcase
+    # puts init.author author
+  end
 end
 
 main
